9aa61c5b1b8203c1013c94ebfab4dfcd5c4dc8ca
AssoDePicche <samuelprado730@gmail.com>
2023-07-20 00:04:57 -0300

    feat: add dependency injection container interface

---
+++ src/Container/Adapter/DependencyInjectionContainer.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Container\Adapter;
+
+interface DependencyInjectionContainer
+{
+    public function autowire(string $className): object;
+
+    /**
+     * @template TClassName
+     * @param class-string<TClassName> $className
+     * @return TClassName
+     */
+    public function get(string $className): object;
+
+    public function set(string $className, callable $definition): self;
+
+    public function singleton(string $className, callable $definition): self;
+}
