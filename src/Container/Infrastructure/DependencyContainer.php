01cb1fc8b96ac8205c0c368bcedc89dad57110aa
AssoDePicche <samuelprado730@gmail.com>
2023-07-20 00:06:06 -0300

    feat: implement dependency injection container

---
+++ src/Container/Infrastructure/DependencyContainer.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Container\Infrastructure;
+
+use Container\Adapter\DependencyInjectionContainer;
+use ReflectionClass;
+use ReflectionParameter;
+
+final class DependencyContainer implements DependencyInjectionContainer
+{
+    private array $definitions = [];
+
+    private array $singletons = [];
+
+    public function autowire(string $className): object
+    {
+        $reflectionClass = new ReflectionClass($className);
+
+        $constructorParameters = array_map(
+            fn (ReflectionParameter $parameter) => $this->get($parameter->getType()->getName()),
+            $reflectionClass->getConstructor()?->getParameters() ?? []
+        );
+
+        return new $className(...$constructorParameters);
+    }
+
+    public function get(string $className): object
+    {
+        if ($instance = $this->singletons[$className] ?? null) {
+            return $instance;
+        }
+
+        $definition = $this->definitions[$className] ?? $this->autowire(...);
+
+        return $definition();
+    }
+
+    public function set(string $className, callable $definition): self
+    {
+        $this->definitions[$className] = $definition;
+
+        return $this;
+    }
+
+    public function singleton(string $className, callable $definition): self
+    {
+        $this->definitions[$className] = function () use ($className, $definition) {
+            $this->singletons[$className] = $definition($this);
+        };
+
+        return $this;
+    }
+}
