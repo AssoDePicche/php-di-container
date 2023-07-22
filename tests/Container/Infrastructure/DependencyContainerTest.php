3b36a74bd1109a7f563f89cd7576768e8885b23e
AssoDePicche <samuelprado730@gmail.com>
2023-07-21 22:29:58 -0300

    test: singleton

--- tests/Container/Infrastructure/DependencyContainerTest.php
+++ tests/Container/Infrastructure/DependencyContainerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Container\Infrastructure;
 
 use Container\Adapter\DependencyInjectionContainer;
+use Mock\SingletonClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use stdClass;
@@ -38,5 +39,21 @@ final class DependencyContainerTest extends TestCase
         $object = $this->container->get($className);
 
         $this->assertInstanceOf($className, $object);
+    }
+
+    #[Test]
+    public function singleton(): void
+    {
+        $this->container->singleton(SingletonClass::class, fn () => new SingletonClass);
+
+        $object = $this->container->get(SingletonClass::class);
+
+        $this->assertInstanceOf(SingletonClass::class, $object);
+
+        $this->assertSame(1, $object::getInstancesCount());
+
+        $sameObject = $this->container->get(SingletonClass::class);
+
+        $this->assertSame(1, $sameObject::getInstancesCount());
     }
 }
